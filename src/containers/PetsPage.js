cb2d7fcf15907f94628916a6cf7bdfa39507b303
Chris Boujoukos <github email address>
2018-06-15 16:35:28 -0400

    Done.

--- src/containers/PetsPage.js
+++ src/containers/PetsPage.js
@@ -9,12 +9,20 @@ import PetsList from '../components/PetsList';
 class PetsPage extends Component {
 
   componentDidMount() {
-    this.props.fetchPets();
+    this.props.onFetchPets();
   }
 
   render() {
+      const { pets, match } = this.props
+      console.log("[containers/PetsPage] this.props:", this.props)
     return (
-      <div>Pets Page</div>
+      <div>
+        <PetsList pets={pets} />
+        <Switch>
+          <Route path={`${match.url}/new`} component={PetsNew} />
+          <Route path={`${match.url}/:petId`} component={PetsShow} />
+        </Switch>
+      </div>
     )
   }
 };
@@ -25,4 +33,10 @@ const mapStateToProps = state => {
   };
 }
 
-export default connect(mapStateToProps, { fetchPets })(PetsPage);
+const mapDispatchToProps = dispatch => {
+  return {
+    onFetchPets: () => dispatch(fetchPets())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PetsPage);
